f8bc73fdc14eff3dced7c80ad04c326433b92c92
LionelArt <lionelart2015@gmail.com>
2017-03-21 01:55:59 +0800

    Webpack Vue 0.1

---
+++ src/app.vue
@@ -0,0 +1,33 @@
+<template>
+	<div id="vue-app">
+		<h1>{{title}}</h1>
+		<ul>
+			<li v-for="n in 5">{{n}}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			title: 'Vue App'
+		}
+	}
+}
+</script>
+
+<style>
+body {
+	margin: 0;
+}
+
+* {
+	margin: 0;
+}
+
+#vue-app {
+	height: 100vh;
+	background-color: #EEE;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// 通用路径
+const PATHS = {
+	src: path.join(__dirname, 'src'),
+	public: path.join(__dirname, 'public')
+};
+
+module.exports = {
+	// 口文件
+	entry: path.resolve(PATHS.src, 'main.js'),
+	output: {
+		// 打包存放目录
+		path: PATHS.public,
+		publicPath: '/',
+		// 打包输出文件名
+		filename: 'build.js'
+	},
+
+	// Loaders
+	module: {
+		rules: [{
+			test: /\.vue$/,
+			use: [{
+				loader: 'vue-loader'
+			}]
+		}, {
+			test: /\.js$/,
+			use: [{
+				loader: 'babel-loader'
+			}]
+		}]
+	},
+
+	// webpack-dev-server是一个轻量的node.js express服务器
+	// webpack-dev-server的作用是用来伺服资源文件，不能替代后端服务器
+	devServer: {
+		// 本地服务器所加载页面所在目录
+		contentBase: './public',
+		// 实时刷新
+		inline: true,
+		// 所有跳转指向index.html
+		historyApiFallback: true,
+		// 监听端口
+		port: 8888,
+		stats: {
+			colors: true
+		}
+	}
+};
